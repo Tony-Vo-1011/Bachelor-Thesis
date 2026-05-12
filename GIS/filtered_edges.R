7e5c2c4ceb3b1cb75ec4f237675a22eaab9bdf55
Tony-Vo-1011 <tonykhanhtoan1011@gmail.com>
2026-05-12 16:00:38 +0200

    Add script to filter duplicate edges and nodes
    This script filters out duplicate and overlapping edges and nodes based on a specified set of node IDs, saving the results to CSV files.

---
+++ GIS/filtered_edges.R
@@ -0,0 +1,17 @@
+# This filter removes all duplicate and overlapping edges and nodes. 
+
+wanted_ids <- unique(nodes_within$node_id)
+
+full_nodes <- read_csv("/Users/tonyvo/Desktop/Thesis/netlogo/data/roads/nodes_scaled_for_netlogo.csv")
+edges <- read_csv("/Users/tonyvo/Desktop/Thesis/netlogo/data/roads/edges_main_component.csv")
+
+filtered_nodes <- full_nodes %>%
+  filter(node_id %in% wanted_ids)
+
+# 3. filter edges where BOTH endpoints are in the selected node set
+filtered_edges <- edges %>%
+  filter(from_id %in% wanted_ids & to_id %in% wanted_ids)
+
+# save
+write_csv(filtered_nodes, "sample_nodes.csv")
+write_csv(filtered_edges, "sample_edges.csv")
